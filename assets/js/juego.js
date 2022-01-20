3049c946c068fea2309773a1f16c32bee28a0dff
ing-jose-rafael <ing.jcarreno@gmail.com>
2022-01-20 13:19:43 -0500

    agregando libreria underscore

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,1 +1,24 @@
-console.log('hola mundo');
+/**
+ * 2C = Two of Clubs (Tréboles)
+ * 2D = Two of Diamonds (Diamantes)
+ * 2H = Two of Hearts (Corazones)
+ * 2S = Two of Spades (Espadas)
+ */
+let deck = [];
+const tipos = ['C','D','H','S']; // almacena los tipos de carta
+const especiales = ['A','J','Q','K']; // almacena la cartas j,q,k 
+
+const crearDeck = ()=>{
+    for (let i = 2; i <= 10; i++) {
+        for (const tipo of tipos) {
+            deck.push(i+tipo);
+        }
+    }
+    for (const especial of especiales) {
+        for (const tipo of tipos) {
+            deck.push(especial+tipo);
+        }
+    }
+    console.log(deck);
+}
+crearDeck();
